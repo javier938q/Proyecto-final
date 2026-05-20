8a1e738addd069afc02f13cd96eacc85b19e50d4
javier938q <javierdiego938@gmail.com>
2026-05-19 19:57:26 -0600

    Refactor voting and scoring logic in examen.c

--- examen.c
+++ examen.c
@@ -12,15 +12,13 @@ de los otros dos datos.*/
 int votos [5][3];
 int contcanciones[10]={0};
 int top3[3];
-int posicion[5];
-int puntos[5];
+int puntos[5]={0};
 int i,j;
 //prototipos de funciones
 int guardado();
 int canciones();
-int ganador(int votos,int top3,int total);
+void puntaje(int total);
 void repeticiones(int total);
-
 
 int main()
 {
@@ -32,13 +30,16 @@ int main()
     }
   printf("Con las canciones mostradas seleccione las tres que mas le gusten en orden desendiente\n\n");
   printf("Para terminar con la votacion ingrese como primer valor el -1\n\n");
+  
   int total= guardado();//nota:aqui es dond ese hace el llamada a la funcion 
+  
   printf("\n--- Resultados guardados ---\n");
   for(int i = 0; i < total; i++) 
   {
     printf("Oyente %d: %d %d %d\n", i+1, votos[i][0], votos[i][1], votos[i][2]);
   }
   repeticiones(total);
+  ganador(total);
   return 0;
 }
 
@@ -70,7 +71,6 @@ Según el ejemplo anterior:*/
 
 void repeticiones(int total)//nota:esta es la funcion de contara los votos y hara el top tres de las canciones
 {
-  int lugar;
     for(i = 0; i < total; i++)
     {
         for(j = 0; j < 3; j++) 
@@ -78,29 +78,32 @@ void repeticiones(int total)//nota:esta es la funcion de contara los votos y har
             contcanciones[votos[i][j]]++;
         }
     }
-    printf("\n\t--- TOP 3 CANCIONES GANADORAS ---\n");
-    for(int lugar = 1; lugar <= 3; lugar++)
+    printf("\n--- TOP 3 CANCIONES mas votadas---\n");
+    int cancionganadora;
+    int maxvotos;
+    int lugar;
+    for(lugar = 0; lugar <3; lugar++)
     {
-          int maxvotos = -1; 
-          int cancionganadora = -1;
-        
-          for(i = 0; i < 10; i++)
-          {
-            if(contcanciones[i] > maxvotos)
-              {
-                  maxvotos = contcanciones[i];
-                  cancionganadora = i;
-                  top3[lugar - 1] = cancionganadora;
-              }
-          }
-    }   
-    if(cancionganadora != -1)
-    {
-       printf("Lugar %d: Cancion numero %d con %d votos\n", lugar, cancionganadora, maxvotos);
-              int top3[lugar-1]=i;
-              contcanciones[cancionganadora] = -1;
-    }       
+      int maxvotos = -1; 
+      int cancionganadora = -1;  
+      for(i = 0; i < 10; i++)
+      {
+        if(contcanciones[i] > maxvotos)
+        {
+           maxvotos = contcanciones[i];
+           cancionganadora = i;
+        }
+      }
+      top3[lugar] = cancionganadora;
+       
+        if(cancionganadora != -1)
+        {
+          printf("Lugar %d: Cancion numero %d con %d votos\n", lugar+1, cancionganadora, maxvotos);
+          printf("\n\n");
+          contcanciones[cancionganadora] = -1;
+        }
     }
+}
 
              
        
@@ -111,31 +114,43 @@ void repeticiones(int total)//nota:esta es la funcion de contara los votos y har
 manera: 30 puntos si entre las 3 canciones votadas está la no. 1, 20 puntos si está la no. 2 y 10 puntos
 suplementarios si han acertado los dos títulos más votados. Se desea obtener el número del oyente que
 más puntos ha obtenido, ya que se le dará un premio.*/
-int ganador(int votos,int top3,int total)
+void puntaje(int total)
 {
-pritf("t---A continuacion se hara la reparticion de punto0s\n");
-for(int i = 0; i < total; i++)
+  int ganador;
+  for(i=0;i<3;i++)
   {
-  for(int j = 0; j < 3; j++)
+    printf("%d\n",top3[i]);
+  }
+  printf("\n\n");
+  printf("%d",total);
+  printf("---A continuacion se hara la reparticion de puntoos---\n");
+  for(i = 0; i < total; i++)
     {
-      if(votos[i][j] ==1)
-            {
-                puntos = puntos + 30; 
-                tiene_top1 = 1;      
-            }
-      if(votos[i][j] == top_canciones[1])
-            {
-                puntos = puntos + 20; 
-                tiene_top2 = 1;      
-            }
-      if(votos[i][j]=top3[0]&&top[1])
-
+    for(j = 0; j < 3; j++)
+      {
+        if(votos[i][j] == 1)
         {
-            puntos = puntos + 10; // Le damos sus 10 puntos extra
-            printf("¡El oyente %d atino al Top 1 y Top 2! (Bono de 10 puntos)\n", i + 1);
+          puntos[i]=puntos[i]+30;
         }
-        printf("El Oyente %d consiguio un total de: %d puntos\n\n", i + 1, puntos);
+        if(votos[i][j] == 2)
+        {
+          puntos[i]=puntos[i]+20;       
+        }
+        if(votos[i][j]==top3[0])
+          {
+              puntos[i]=puntos[i]+10; // Le damos sus 10 puntos extra
+          }
+       }
+       printf("El Oyente %d consiguio un total de: %d puntos\n\n", i + 1, puntos[i]);
+        if(gandor)
     }
+}
+void puntaje()
+{
+  printf("El ganador del concurso es ");
+}
+  
+  
   
 
     
